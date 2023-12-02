a351c236db6474d06a57077b84b048e21da308f5
Sangwun-Ko <kosang5653@naver.com>
2023-12-02 21:27:35 +0900

    Create Customer.js

--- management/src/App.js
+++ management/src/App.js
@@ -1,11 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { Customer } from './components/Customer.js';
+
+const customers = [{
+  'id': 1,
+  'image': 'https://picsum.photos/64/64',
+  'name': '고상운',
+  'birthday': '940522',
+  'gender': '남자',
+  'job': '학생'
+},
+{
+  'id': 2,
+  'image': 'https://picsum.photos/64/64',
+  'name': '고상민',
+  'birthday': '920817',
+  'gender': '남자',
+  'job': '직업'
+},
+{
+  'id': 3,
+  'image': 'https://picsum.photos/64/64',
+  'name': '고상인',
+  'birthday': '970421',
+  'gender': '남자',
+  'job': '경찰'
+}]
 
 function App() {
   return (
-    <div className="gray-background">
-        <img src={logo} lat="logo"/>
-        <h2>Let's develop management system!</h2>
+    <div>
+      {customers.map(c => { return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/> ) })      }
     </div>
   );
 }
